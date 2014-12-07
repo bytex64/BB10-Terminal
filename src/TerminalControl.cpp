56c61e276925b516c4045e971a497b598c9cf7e8
Matt Erickson <matt@spideroak.com>
2014-12-06 23:48:10 -0600

    Fixed ScrollView width, starting to setup for width calculation.

--- src/TerminalControl.cpp
+++ src/TerminalControl.cpp
@@ -8,11 +8,14 @@
 #include "TerminalControl.h"
 
 #include <bb/cascades/Container>
+#include <bb/cascades/HorizontalAlignment>
 #include <bb/cascades/Label>
 #include <bb/cascades/ScrollView>
 #include <bb/cascades/ScrollAnimation>
 #include <bb/cascades/SystemDefaults>
 #include <bb/cascades/TextStyle>
+
+#include <QtGui/QFontMetrics>
 
 using namespace bb::cascades;
 
@@ -20,15 +23,14 @@ TerminalControl::TerminalControl(Container *parent)
     : CustomControl(parent)
 {
     m_rootContainer = new Container();
+
     m_scrollView = new ScrollView();
+    m_scrollView->setHorizontalAlignment(HorizontalAlignment::Fill);
+
     m_label = new Label();
-
     m_label->setMultiline(true);
-
-    TextStyle *monospaceStyle = new TextStyle(SystemDefaults::TextStyles::bodyText());
-    monospaceStyle->setFontFamily("\"DejaVu Sans Mono\", monospace");
-
-    m_label->textStyle()->setBase(*monospaceStyle);
+    m_label->setHorizontalAlignment(HorizontalAlignment::Fill);
+    setTerminalFont();
 
     m_scrollView->setContent(m_label);
     m_rootContainer->add(m_scrollView);
@@ -48,4 +50,16 @@ void TerminalControl::update(QString data)
 
     // Dirty hack. Do better later.
     m_scrollView->scrollToPoint(0, 999999, ScrollAnimation::Smooth);
+}
+
+void TerminalControl::calculateWindowSize()
+{
+
+}
+void TerminalControl::setTerminalFont()
+{
+    TextStyle *monospaceStyle = new TextStyle(SystemDefaults::TextStyles::bodyText());
+    monospaceStyle->setFontFamily("\"DejaVu Sans Mono\", monospace");
+
+    m_label->textStyle()->setBase(*monospaceStyle);
 }
